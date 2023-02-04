3dfffa49c77b6f298326754d8c2f6caaadbe60c2
Sanders76 <sanders76@list.ru>
2023-02-04 14:57:37 +0300

    Updated code at 2.3.py

--- 2.3.py
+++ 2.3.py
@@ -16,9 +16,10 @@
 # 10000  -> 1,2,4,8,16,32,64,128,256,512,1024,2048,4096,8192,
 
 
-n = int(input(f'Введите число N: ')
+n = int(input(f'Введите число N: '))
 current_power_of_two = 0
 while 2**current_power_of_two <= n:
   print(2**current_power_of_two)
-  print(*(2**current_power_of_two), sep=', ')
+  current_power_of_two += 1
+  # print(*(2**current_power_of_two), sep=', ')
   # или попробовать заменить 2**current_power_of_two на отдельную переменную.
