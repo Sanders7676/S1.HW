8941d364084f1fd93c3be61fd2a822b685264386
Sanders76 <sanders76@list.ru>
2023-02-08 22:06:18 +0300

    Updated code in 3.2.py

--- 3.2.py
+++ 3.2.py
@@ -9,76 +9,25 @@
 # Input: [10, 5, 7, 3, 3, 2, 5, 7, 3, 8], X = 9
 
 
-# available_list = [10, 5, 7, 3, 3, 2, 5, 7, 3, 8]
-# given_number_X = int(input(f'Введите число X: '))
 
-# for el in available_list:
-#     if el == given_number_X:
-#         print(given_number_X)
-#         break
-        
-
-# list_2 = []
-# # q = 0
-# i = 0
-# for el in available_list:
-#     if el != given_number_X:
-#         if available_list[i] > given_number_X:
-#             q = available_list[i] - given_number_X
-#         else:
-#             q = given_number_X - available_list[i]
-        
-#         list_2.append(q)
-#         i += 1
-
-# print(list_2)
-
-
-# min_element_in_list_2 = list_2[0]
-
-# j = 0
-
-# # if available_list[]
-
-# for j in list_2:
-#     if list_2[j] < min_element_in_list_2:     # Показывает, что здесь ошибка, когда Х = 0
-#         min_element_in_list_2 = list_2[j]
-
-# print(available_list)
-# print(min_element_in_list_2)
-# print(available_list[j])
-
-
-
-# Вариант решения после обработки всего предыдущего:
-
-available_list = [10, 5, 7, 3, 3, 2, 5, 7, 3, 8]        # Если Х = 0, то ответ - 2
+available_list = [10, 5, 7, 3, 3, 2, 5, 7, 3, 8]
 given_number_X = int(input(f'Введите число X: '))
 
-list_2 = []
+list_of_differences = []
 
-for i in range(len(available_list)):
-# for i in available_list:    
-    if available_list[i] > given_number_X:
-        q = available_list[i] - given_number_X
+for current_index in range(len(available_list)):
+    if available_list[current_index] > given_number_X:
+        difference = available_list[current_index] - given_number_X
     else:
-        q = given_number_X - available_list[i]
-    list_2.append(q)
-print(list_2)                                       # [10, 5, 7, 3, 3, 2, 5, 7, 3, 8]
+        difference = given_number_X - available_list[current_index]
+    list_of_differences.append(difference)
 
-min_element_in_list_2 = list_2[0]
-ind = 0
+min_element_in_list_of_differences = list_of_differences[0]
+desired_index_in_available_list= 0
 
-for i in range(1, len(list_2)):
-# for i in range(len(list_2)):    
-# for i in list_2:   
-    if list_2[i] < min_element_in_list_2:     
-        min_element_in_list_2 = list_2[i]
-        ind = i
+for current_index in range(1, len(list_of_differences)):
+    if list_of_differences[current_index] < min_element_in_list_of_differences:     
+        min_element_in_list_of_differences = list_of_differences[current_index]
+        desired_index_in_available_list = current_index
 
-print(available_list)               # Для проверки
-print(min_element_in_list_2)        # Для проверки
-print(available_list[ind])          # Это ответ.
-
-
-
+print(available_list[desired_index_in_available_list])
