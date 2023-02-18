6a190411318252dddaae2f78b7f2c6ec345d0b02
Sanders76 <sanders76@list.ru>
2023-02-18 14:54:08 +0300

    Updated 6.1_(39).py

--- Seminars_online/6.1_(39).py
+++ Seminars_online/6.1_(39).py
@@ -16,9 +16,22 @@
 # <function_name>([3, 4, 1, 5, 1, 3, 10, 4, 9, 5], [9, 6, 6, 5, 10, 1, 10, 9, 1, 5] ) -> [3, 4, 3, 4]
 
 
-def intersection_set(l: list, l_1: list) -> list:
+def diff_set(l: list, l_1: list) -> list:
     new_set = set(l).difference(set(l_1))
     return list(new_set)
 
-print(intersection_set([3, 1, 3, 4, 2, 4, 12], [4, 15, 43, 1, 15, 1]))
-print(intersection_set([3, 4, 1, 5, 1, 3, 10, 4, 9, 5], [9, 6, 6, 5, 10, 1, 10, 9, 1, 5]))
+print(diff_set([3, 1, 3, 4, 2, 4, 12], [4, 15, 43, 1, 15, 1]))
+print(diff_set([3, 4, 1, 5, 1, 3, 10, 4, 9, 5], [9, 6, 6, 5, 10, 1, 10, 9, 1, 5]))
+
+
+# Через списки
+
+def diff_list(l: list, l_1: list) -> list:
+    result = []
+    for element in l:
+        if element not in l_1 and element not in result:
+            result.append(element)
+    return result
+
+print(diff_list([3, 1, 3, 4, 2, 4, 12], [4, 15, 43, 1, 15, 1]))
+print(diff_list([3, 4, 1, 5, 1, 3, 10, 4, 9, 5], [9, 6, 6, 5, 10, 1, 10, 9, 1, 5]))
