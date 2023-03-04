bb2c795c1e6a23e09d97498f3688d2b52f6451fb
Sanders76 <sanders76@list.ru>
2023-03-04 15:24:17 +0300

    Updated 8.1_(49).py

--- Seminars_online/8.1_(49).py
+++ Seminars_online/8.1_(49).py
@@ -16,3 +16,44 @@
 # Усложнение.
 # - Сделать тесты для функций
 # - Разделить на model-view-controller
+
+id_client = 0
+# phone_book = {123: ('Нехаев', "Михаил", "8900111", "друг"),
+# 124: ("Петров", "Сергей", "8901222", "враг")}
+
+phone_book = {}
+
+def menu(data: dict, id_client: int):
+    while True:
+        print('Выберите действие: ')
+        print('1 - создать новую запись')
+        print('2 - распечатать содержимое справочника')
+        print('3 - импортировать данные с текстового файла')
+        get = input('Введите действие: ')
+        if get == '':
+            print('До свидания!')
+            break
+        elif get == '1':
+            id_client += 1
+            data = create(data, id_client, get_data())
+        elif get == '2':
+            print_phone_book(data)
+        else:
+            print('Некорректный ввод данных, введите ещё раз: ')
+
+def create(data: dict, id: int, elem: tuple) -> dict: # добавляет запись в существующую телефонную книгу
+    data[id] = elem
+    return data
+
+def print_phone_book(data: dict) -> None:
+    for key, values in data.items():
+        print(f'Идентификатор: {key}, {values}')
+
+def get_data() -> tuple: # запрашивает данные у пользователя
+    surname = input('Введите фамилию: ')
+    name = input('Введите имя: ')
+    phone = input('Введите номер телефона: ')
+    discription = input('Введите описание: ')
+    return (surname, name, phone, discription)
+
+menu(phone_book, id_client)
